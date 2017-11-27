c92202b770bcf4f8fd5c6b2887fece4ffc6ccff8
wenliang303 <64457570@qq.com>
2017-11-27 23:58:39 +0800

    add

---
+++ NetEasePromgraming/Class2Week2/gradient_checking.py
@@ -0,0 +1,58 @@
+#encoding=utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import math
+import sklearn
+import sklearn.datasets
+
+from opt_utils import load_params_and_grads, initialize_parameters, forward_propagation, backward_propagation
+from opt_utils import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
+from testCases import *
+
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+# GRADED FUNCTION: update_parameters_with_gd
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    """
+    Update parameters using one step of gradient descent
+    
+    Arguments:
+    parameters -- python dictionary containing your parameters to be updated:
+                    parameters['W' + str(l)] = Wl
+                    parameters['b' + str(l)] = bl
+    grads -- python dictionary containing your gradients to update each parameters:
+                    grads['dW' + str(l)] = dWl
+                    grads['db' + str(l)] = dbl
+    learning_rate -- the learning rate, scalar.
+    
+    Returns:
+    parameters -- python dictionary containing your updated parameters 
+    """
+
+    L = len(parameters) // 2 # number of layers in the neural networks
+
+    # Update rule for each parameter
+    for l in range(L):
+        ### START CODE HERE ### (approx. 2 lines)
+        parameters["W" + str(l+1)] = grads["dW" + str(l+1)]
+        parameters["b" + str(l+1)] = grads["db" + str(l+1)]
+        ### END CODE HERE ###
+        
+    return parameters
+
+#############################################
+def test_update_parameters_with_gd():
+    parameters, grads, learning_rate = update_parameters_with_gd_test_case()
+
+    parameters = update_parameters_with_gd(parameters, grads, learning_rate)
+    print("W1 = " + str(parameters["W1"]))
+    print("b1 = " + str(parameters["b1"]))
+    print("W2 = " + str(parameters["W2"]))
+    print("b2 = " + str(parameters["b2"]))
+
+test_update_parameters_with_gd()
---
+++ NetEasePromgraming/Class2Week3/demo.py
@@ -0,0 +1,43 @@
+#encoding=utf-8
+
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from tf_utils import load_dataset, random_mini_batches, convert_to_one_hot, predict
+
+
+np.random.seed(1)
+
+def test_init():
+    y_hat = tf.constant(36, name="y_hat")
+    y = tf.constant(39, name='y')
+    loss = tf.Variable((y-y_hat) ** 2, name='loss')
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as session:
+        session.run(init)
+        print(session.run(loss))
+
+def test_multiply():
+    a = tf.constant(3)
+    b = tf.constant(30)
+    c = tf.multiply(a, b)
+
+    with tf.Session() as session:
+        session.run(init)
+        print(session.run(c))
+
+def test_feed():
+    sess = tf.Session()
+    xx = tf.placeholder(tf.int32, name='any_name')
+    res = sess.run(xx * 2, feed_dict={xx:30})
+    print(a.__dict__.items())
+    sess.close()
+
+#################################
+test_init()
+test_multiply()
+test_feed()
