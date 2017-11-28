d430a889e2dc59417d8b5ee771598b1fa0996f6a
wenliang303 <64457570@qq.com>
2017-11-28 13:04:36 +0800

    test TensorFlow

--- NetEasePromgraming/Class2Week3/TensorFlowTutorial.py
+++ NetEasePromgraming/Class2Week3/TensorFlowTutorial.py
@@ -98,17 +98,17 @@ def sigmoid(z):
 # GRADED FUNCTION: cost
 def cost(logits, labels):
     """
-    Computes the cost using the sigmoid cross entropy
-    
-    Arguments:
-    logits -- vector containing z, output of the last linear unit (before the final sigmoid activation)
-    labels -- vector of labels y (1 or 0) 
+    Computes the cost using the sigmoid cross entropy
+
+    Arguments:
+    logits -- vector containing z, output of the last linear unit (before the final sigmoid activation)
+    labels -- vector of labels y (1 or 0) 
     
     Note: What we've been calling "z" and "y" in this class are respectively called "logits" and "labels" 
     in the TensorFlow documentation. So logits will feed into z, and labels into y. 
-    
-    Returns:
-    cost -- runs the session of the cost (formula (2))
+    
+    Returns:
+    cost -- runs the session of the cost (formula (2))
     """
     
     ### START CODE HERE ### 
