96dab4d8e9c197913d1495cf2574e1027eddc980
wenliang303 <64457570@qq.com>
2017-11-28 15:56:41 +0000

    add comment

--- NetEasePromgraming/Class1Week2/main.py
+++ NetEasePromgraming/Class1Week2/main.py
@@ -366,9 +366,9 @@ def testImg():
     my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
     my_predicted_image = predict(d["w"], d["b"], my_image)
     
-    plt.figure()
-    plt.imshow(image)
-    plt.show()
+    #plt.figure()
+    #plt.imshow(image)
+    #plt.show()
     print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
 
 #testModel()
