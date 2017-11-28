f1b8814b47437a37212b9d8f5b375857e932bed2
wenliang303 <64457570@qq.com>
2017-11-28 17:03:40 +0800

    test TensorFlow

--- NetEasePromgraming/Class2Week3/TensorFlowTutorial.py
+++ NetEasePromgraming/Class2Week3/TensorFlowTutorial.py
@@ -508,8 +508,18 @@ def test_compute_cost():
 
 def test_model():
     # Loading the dataset
-
     X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+    # Flatten the training and test images
+    X_train_flatten = X_train_orig.reshape(X_train_orig.shape[0], -1).T
+    X_test_flatten = X_test_orig.reshape(X_test_orig.shape[0], -1).T
+    # Normalize image vectors
+    X_train = X_train_flatten/255.
+    X_test = X_test_flatten/255.
+    # Convert training and test labels to one hot matrices
+    Y_train = convert_to_one_hot(Y_train_orig, 6)
+    Y_test = convert_to_one_hot(Y_test_orig, 6)
+
     parameters = model(X_train, Y_train, X_test, Y_test)
 
 test_linear_function()
