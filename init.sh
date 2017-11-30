c6434ae3c1d2325962223b8d4edfe898fe894e13
wenliang303 <64457570@qq.com>
2017-11-30 17:32:53 +0800

    env init shell

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 echo "start init"  
-apt-get  update 
 rm -rf /usr/bin/python
 ln -s /usr/bin/python3.5 /usr/bin/python
 apt-get install python-pip
+apt-get install python3-pip
 apt-get install python3-tk
 git config --global user.name "wenliang303"
 git config --global user.email 64457570@qq.com
