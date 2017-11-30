32accf097df7329ae71ac6d1d1142c57fcc1a36d
wenliang303 <64457570@qq.com>
2017-11-30 16:57:31 +0800

    env init shell

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash  
+echo "start init"  
+apt-get  update 
+rm -rf /usr/bin/python
+ln -s /usr/bin/python3.5 /usr/bin/python
+apt-get install python-pip
+apt-get install python3-tk
+git config --global user.name "wenliang303"
+git config --global user.email 64457570@qq.com
+pip install matplotlib
+pip install h5py
+pip install tensorflow
+pip install scipy
+pip install scikit-learn
+pip install pillow
+pip install Ipython
+pip install pydot
+pip install keras
